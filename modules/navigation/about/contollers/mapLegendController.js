b37fb96876da5e2ea984ce5c7637f52e0ee9ad9a
gbhalsing-compumatrice <gbalsing@edrnet.com>
2015-06-04 18:16:44 +0530

    added new code

---
+++ modules/navigation/about/contollers/mapLegendController.js
@@ -0,0 +1,5 @@
+﻿angular.module('EDRLightbox').controller('mapLegendController', ['$scope', '$state', '$location', '$document', '$timeout', '$http', '$filter', 'NavigationService', '$stateParams', function ($scope, $state, $location, $document, $timeout, $http, $filter, NavigationService, $stateParams) {
+
+
+
+}]);
