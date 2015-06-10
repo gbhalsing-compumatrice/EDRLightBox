9033698227a5d6b13a0d1108ee075953867681ad
gbhalsing-compumatrice <gbalsing@edrnet.com>
2015-06-10 17:11:05 +0530

    deleted old code

--- modules/navigation/about/contollers/disclaimerController.js
+++
@@ -1,5 +0,0 @@
-﻿angular.module('EDRLightbox').controller('disclaimerController', ['$scope', '$state', '$location', '$document', '$timeout', '$http', '$filter', 'NavigationService', '$stateParams', function ($scope, $state, $location, $document, $timeout, $http, $filter, NavigationService, $stateParams) {
-
-
-
-}]);
--- modules/navigation/about/contollers/feedbackController.js
+++
@@ -1,5 +0,0 @@
-﻿angular.module('EDRLightbox').controller('feedbackController', ['$scope', '$state', '$location', '$document', '$timeout', '$http', '$filter', 'NavigationService', '$stateParams', function ($scope, $state, $location, $document, $timeout, $http, $filter, NavigationService, $stateParams) {
-
-
-
-}]);
