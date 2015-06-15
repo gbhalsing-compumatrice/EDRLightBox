1acdf71319610ac63f9915bfc2d32feaf338894e
gbhalsing-compumatrice <gbalsing@edrnet.com>
2015-06-15 17:17:22 +0530

    modified Database description page

--- modules/navigation/about/contollers/dbDescriptionsController.js
+++ modules/navigation/about/contollers/dbDescriptionsController.js
@@ -1,5 +1,36 @@
-﻿angular.module('EDRLightbox').controller('dbDescriptionsController', ['$scope', '$state', '$location', '$document', '$timeout', '$http', '$filter', 'DashboardService', '$stateParams', function ($scope, $state, $location, $document, $timeout, $http, $filter, DashboardService, $stateParams) {
+﻿angular.module('EDRLightbox').controller('dbDescriptionsController', ['$scope', '$state', '$location', '$document', '$timeout', '$http', '$filter', 'NavigationService', '$stateParams', function ($scope, $state, $location, $document, $timeout, $http, $filter, NavigationService, $stateParams) {
 
+
+    bindDBData();
+
+
+    function bindDBData() {
+
+        NavigationService.getDBDescriptionsData().then(function (result) {
+            
+            $scope.DBDescriptionData = result;
+        }, function (error) {
+
+        });
+    }
+
+    $scope.searchDatabase = function () {
+
+        
+        //var data = findRecord($scope.SearchText.$);
+        
+
+
+    }
+
+    //function findRecord(searchtext) {
+    //    for (var i = 1; i < $scope.DBDescriptionData.length; i++) {
+    //        console.log("find data " + JSON.stringify($scope.DBDescriptionData[i]);
+    //        if ($scope.DBDescriptionData[i] == searchtext) {
+    //            return $scope.DBDescriptionData[i];
+    //        }
+    //    }
+    //}
 
 
 }]);
