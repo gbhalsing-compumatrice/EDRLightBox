89a1338f18276294c897e46ca1131f14c927d7d1
gbhalsing-compumatrice <gbalsing@edrnet.com>
2015-06-15 16:58:58 +0530

    pushed updated code

---
+++ modules/dashboard/controllers/infoPopupController.js
@@ -0,0 +1,18 @@
+﻿angular.module('EDRLightbox').controller('infoPopupController', ['$scope', '$state', '$location', '$document', '$timeout', '$http', '$filter', 'NgTableParams', 'DashboardService', '$stateParams', '$window', '$modal', function ($scope, $state, $location, $document, $timeout, $http, $filter, NgTableParams, DashboardService, $stateParams, $window,$modal) {
+    var modalInstance;
+
+    modalInstance = $modal.open({
+        templateUrl: 'info.html',
+        scope: $scope,
+        //controller: infoPopupController,
+        size: 'md'
+
+    })
+
+    $scope.cancel = function () {
+
+       $modalInstance.close();
+
+    }
+
+}]);
---
+++ modules/navigation/about/contollers/disclaimerController.js
@@ -0,0 +1,5 @@
+﻿angular.module('EDRLightbox').controller('disclaimerController', ['$scope', '$state', '$location', '$document', '$timeout', '$http', '$filter', 'DashboardService', '$stateParams', function ($scope, $state, $location, $document, $timeout, $http, $filter, DashboardService, $stateParams) {
+
+
+
+}]);
