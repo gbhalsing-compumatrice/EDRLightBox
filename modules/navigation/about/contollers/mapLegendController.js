b797a069eaa51274b26aac6b4b4dd64eb5bbe466
gbhalsing-compumatrice <gbalsing@edrnet.com>
2015-06-17 18:36:47 +0530

    deleted module folder

--- modules/navigation/about/contollers/mapLegendController.js
+++
@@ -1,5 +0,0 @@
-﻿angular.module('EDRLightbox').controller('MaplegendController', ['$scope', '$state', '$location', '$document', '$timeout', '$http', '$filter', 'DashboardService', '$stateParams', function ($scope, $state, $location, $document, $timeout, $http, $filter, DashboardService, $stateParams) {
-
-
-
-}]);
