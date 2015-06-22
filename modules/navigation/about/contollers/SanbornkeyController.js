1c1be7409680dbf7fc71126644e58c138d114104
gbhalsing-compumatrice <gbalsing@edrnet.com>
2015-06-22 18:24:55 +0530

    deleted old code

--- modules/navigation/about/contollers/SanbornkeyController.js
+++
@@ -1,5 +0,0 @@
-﻿angular.module('EDRLightbox').controller('SanbornkeyController', ['$scope', '$state', '$location', '$document', '$timeout', '$http', function ($scope, $state, $location, $document, $timeout, $http) {
-
-
-
-}]);
