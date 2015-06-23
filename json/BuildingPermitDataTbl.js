ec8555e842744697f63ee4899d46dd2010bae96d
gbhalsing-compumatrice <gbalsing@edrnet.com>
2015-06-23 17:57:28 +0530

    deleted code

--- json/BuildingPermitDataTbl.js
+++
@@ -1,8 +0,0 @@
-﻿[
-       { "Id": "1", "address": "1340 W CENTRAL BLVD", "date": "Jan 23 2007", "type": "", "use": "COMMERCIAL", "actual": "100 FT ASL", "epaID": "N/A", "edrID": "S105620237" },
-       { "Id": "2", "address": "1320 W CENTRAL BLVD", "date": "Oct 1 2008", "type": "", "use": "Commercial", "actual": "100 FT ASL", "epaID": "N/A", "edrID": "S105620237" },
-       { "Id": "3", "address": "27 N NASHVILLE AVE", "date": "Jan 14 2005", "type": "ROOF", "use": "", "actual": "100 FT ASL", "epaID": "N/A", "edrID": "S105620237" },
-       { "Id": "4", "address": "1331 W PINE ST", "date": "Jan 23 2007", "type": "Fence", "use": "COMMERCIAL", "actual": "100 FT ASL", "epaID": "N/A", "edrID": "S105620237" },
-       { "Id": "5", "address": "1320 W PINE ST", "date": "Oct 17 2005", "type": "ACCESSORY", "use": "", "actual": "100 FT ASL", "epaID": "N/A", "edrID": "S105620237" }
-
-]
--- json/SoilDataTbl.js
+++
@@ -1,8 +0,0 @@
-﻿[
-       { "soilId": "1", "soiltype": "Milpitas", "Texture": "Fine Sandy Loam", "hydrologicGroup": "Class D", "Drainage": "Moderately Well" },
-       { "soilId": "2", "soiltype": "Milpitas", "Texture": "Stone Fine Sandy Loam", "hydrologicGroup": "Class D", "Drainage": "Moderately Well" },
-       { "soilId": "3", "soiltype": "Ballard", "Texture": "Fine Sandy Loam", "hydrologicGroup": "Class B", "Drainage": "Well" },
-       { "soilId": "4", "soiltype": "Elder", "Texture": "Sandy Loam", "hydrologicGroup": "Class B", "Drainage": "Well" },
-       { "soilId": "5", "soiltype": "Milpitas", "Texture": "Stone Fine Sandy Loam", "hydrologicGroup": "Class D", "Drainage": "Moderately Well" }
-
-]
