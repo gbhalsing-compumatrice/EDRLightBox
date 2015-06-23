a183cacf7ab5613f468236e075537923cebaf673
gbhalsing-compumatrice <gbalsing@edrnet.com>
2015-06-23 18:23:40 +0530

    added code

---
+++ json/ColumnFilterList.js
@@ -0,0 +1,13 @@
+﻿{
+    "status": {
+        "one": true, "two": true, "three": true, "four": true, "five": true, "six": true
+    },
+        "names": {
+            "col1": "Site Name", "col2": "Address", "col3": "Databases", "col4": "Relative Elevation", "col5": "Distance(ft)", "col6": "Direction"
+        },
+    "db": {
+        "RCRA-LQG": true, "RCRA-SQG": true, "RCRA-CESQG": true, "LUST": true, "SLIC": true, "UST": true, "CA FID UST": true,
+        "HIST UST": true, "SWEEPS UST": true, "EDR US Hist Cleaners": true, "EDR US Hist Auto Stat": true, "CUPA Listings": true,
+        "DRYCLEANERS": true, "FINDS": true, "HIST UST": true, "Include Orphans in Site List": true
+    }
+}
---
+++ modules/navigation/about/contollers/SanbornkeyController.js
@@ -0,0 +1,5 @@
+﻿angular.module('EDRLightbox').controller('SanbornkeyController', ['$scope', '$state', '$location', '$document', '$timeout', '$http', function ($scope, $state, $location, $document, $timeout, $http) {
+
+
+
+}]);
