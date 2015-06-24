1d254faca40026987aeeb2e475e89694d24474d6
gbhalsing-compumatrice <gbalsing@edrnet.com>
2015-06-24 12:17:40 +0530

    added updated code

---
+++ json/AddressList.js
@@ -0,0 +1,11 @@
+﻿[
+       
+       { "Id": "1", "address": "MISSION ST", "count": "2", "active": "true" },
+       { "Id": "2", "address": "W MISSION ST", "count": "2", "active": "true" },
+       { "Id": "3", "address": "DE LA VINA ST", "count": "2", "active": "true" },
+       { "Id": "4", "address": "PUEBLO AND BATH", "count": "4", "active": "true" },
+       { "Id": "5", "address": "CHAPALA ST", "count": "1", "active": "true" },
+       { "Id": "6", "address": "DE LA GUERTA ST", "count": "2", "active": "true" },
+       { "Id": "7", "address": "STATE ST", "count": "2", "active": "true" },
+       { "Id": "8", "address": "DOWNTOWN SANTA BARBARA", "count": "3", "active": "true" }
+]
---
+++ json/NotesDataTbl.js
@@ -0,0 +1,8 @@
+﻿[
+       { "Id": "1", "note": "AST found here that was not listed in the radius map" },
+       { "Id": "2", "note": "Evidence of some type of spill here. Could be engine oil" },
+       { "Id": "3", "note": "Need to follow up with site manager about this" },
+       { "Id": "4", "note": "Improper storage of chemicals" },
+       { "Id": "5", "note": "main site signage" }
+
+]
